0c97487c7efb3d68a761707abc23ded81fcc17ea
mardi_d <mduckworth@gmail.com>
2025-02-07 15:09:14 -0800

    First attempt Try and Except error handling

--- error-handling-with-try-and-except/main.py
+++ error-handling-with-try-and-except/main.py
@@ -3,3 +3,8 @@ print("Which two numbers do you want to add together?")
 user_input1 = input("Input your first number: ")
 user_input2 = input("Input your second number: ")
 
+try:
+    print(float(user_input1) + float(user_input2))
+
+except:
+    print('At least one of the inputs provided was NOT a number.')
