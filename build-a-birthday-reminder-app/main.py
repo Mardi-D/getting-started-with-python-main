236f09575be0ac9650349efd325bbdcdc912cefb
mardi_d <mduckworth@gmail.com>
2025-02-07 13:04:21 -0800

    First commit birthday reminder app

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,12 @@
+
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "James":
+    print("Happy Birthday, I love you so much! Today February 26th!")
+elif user_input == "Camille":
+    print("Happy Birthday SweetPea, today is August 5th!")
+elif user_input == "Dellie Doo":
+    print("Happy Birthday Twinkie, today is Sept 30th")
+else: 
+    print("Can't find a birth for this person")
+
---
+++ math-can-be-fun-with-python/main.py
@@ -0,0 +1,11 @@
+
+print(60 / 23)
+print(20 * 3 + 8)
+
+pay = 3800
+expenses = 2200
+
+print(expenses - pay)
+
+
+#MD complete..submit to git
