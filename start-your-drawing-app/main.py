85f92ecd17302b09e3d9b0b40772d81f18d2d1f8
mardi_d <mduckworth@gmail.com>
2025-02-10 14:02:07 -0800

    First commit for start your drawing app

---
+++ start-your-drawing-app/main.py
@@ -0,0 +1,76 @@
+# import turtle
+
+# t = turtle.Turtle()
+# c = t.color()
+
+# t.forward(100)
+# t.rotate(60)
+
+
+
+# for c in ['red', 'green', 'blue', 'yellow']:
+#   t.color(c)
+#   t.forward(75)
+#   t.left(90)
+
+import turtle as turtle
+
+#Hexagon
+# turtle.pencolor("green")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("blue")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("yellow")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("red")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("orange")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("black")
+# turtle.forward(100)
+# turtle.right(60)
+
+
+
+#Drawing a square
+
+#choose color
+# turtle.pencolor("blue")
+
+# #directions for a square
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+
+#star
+
+turtle.pencolor("red")
+
+turtle.forward(110)
+turtle.right(216)
+turtle.forward(110)
+turtle.right(216)
+turtle.forward(110)
+turtle.right(216)
+turtle.forward(110)
+turtle.right(216)
+turtle.forward(110)
+turtle.right(216)
+
+#to keep python window from closing /keep python window open
+turtle.done()
