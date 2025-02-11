1fad8920af9498185a4af1105d8bcfb44bdda5ed
mardi_d <mduckworth@gmail.com>
2025-02-11 14:34:46 -0800

    First commit for clean up this list

--- clean-up-this-list/main.py
+++ clean-up-this-list/main.py
@@ -1,1 +1,20 @@
-websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+#list of favorite websites
+websites = [
+    "www.google.com",
+    "www.youtube.com",
+    "www.dropbox.com",
+    "www.quora.com",
+    "www.reddit.com",
+    "www.instagram.com",
+    ]
+#remove website from index 2 of list
+websites.pop(2)
+
+#add another website using insert()
+websites.insert(2, "www.yahoo.com")
+
+
+#adding website to the end of the list
+websites.append("www.bluesky.com")
+
+print(websites)
