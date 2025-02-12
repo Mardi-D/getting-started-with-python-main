02d1c3d6c11fb5ff45a6bcf4ba32a227b777dab6
mardi_d <mduckworth@gmail.com>
2025-02-11 18:35:26 -0800

    First commit for iterate through numbers

--- iterate-through-numbers/main.py
+++ iterate-through-numbers/main.py
@@ -1,2 +1,17 @@
 years_list = [5,7,12,13,9,21,11,10]
 num_eligible = 0
+
+for years in years_list:
+    if years > 9:
+        num_eligible += 1
+print(num_eligible)
+
+
+
+# # my non pythonic code...lol
+# for years in years_list:
+#     if years >= 9:
+#         print(f'Congratulations on your {years}th year anniversary!')
+#     else:
+#         print("Keep working, ya'll got a ways to go.")
+
