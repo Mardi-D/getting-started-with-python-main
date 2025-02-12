dcda0bdc53b1867ac42429b61e498711a6555293
mardi_d <mduckworth@gmail.com>
2025-02-12 11:30:30 -0800

    First commit for build todo list

--- build-a-to-do-list/main.py
+++ build-a-to-do-list/main.py
@@ -3,3 +3,14 @@ done = False
 
 print("Type 'done' at any time to exit")
 
+while not done:
+    new_item = input("Please add items to your to do list: ")
+    
+    if new_item == "done":
+        done  == True
+    else:
+      todo_list.append(new_item)
+
+    print(todo_list)
+
+
