7a15872d9de612bea44da29880ec5f88b80f64fd
mardi_d <mduckworth@gmail.com>
2025-02-13 15:14:08 -0800

    First commit for language translator

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -1,9 +1,24 @@
 translations = {
-  "hello":"hola",
-  "thank you":"gracias",
-  "sorry":"lo siento"
+  "hello":        "hola",
+  "thank you":    "gracias",
+  "sorry":        "lo siento",
+  "I'm hungry":   "yo tengo hambre",
+  "I love you":   "te amo mucho"
 }
 
 done = False
 
 print('Type "done" at any time to exit')
+
+while not done:
+    word = input('Please type an English word to translate to Spanish: ').lower()
+
+    if word == 'done':
+        done = True
+    elif word in translations:
+        print(translations[word])
+    else: 
+        print(f'{word} is not in the translations dictionary. Please try a different word.')
+        
+
+
