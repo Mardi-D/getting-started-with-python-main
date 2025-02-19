8e87a484cb1ccc68bc361eb56ad43e3265525f40
mardi_d <mduckworth@gmail.com>
2025-02-19 12:47:07 -0800

    First commit for start your personal assistant class

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -1,10 +1,23 @@
 # Give the class a name
-class:
-  # Add an __init__ function here
+class PersonalAssistant:
+# Add an __init__ function here
+  def __init__(self, contacts, todos):
+    self.contacts = {
+      'Ann': 'Marketing Coordinator', 
+      'Chelsea': 'Software Developer', 
+      'Nichole': 'Sales Representative', 
+      'Max': 'Technical Writer'
+    }
+    self.todos = []
 
   # Complete the get_contact function code
   def get_contact(self, name):
-
+    if name in self.contacts:
+      return self.contacts[name]
+    else:
+      return 'There is not contact information for the name provided.'
+    
+  
 
 # Code to test output of the class
 assistant = PersonalAssistant()
